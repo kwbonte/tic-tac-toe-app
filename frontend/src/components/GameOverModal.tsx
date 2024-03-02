39457273dc97535bb5d1ab01412d9df3c82b07fd
Kenneth Bonte <Kenneth.Bonte@indigobeam.com>
2024-03-02 16:57:57 -0500

    Adding in some fun pops of color on game over win case

--- frontend/src/components/GameOverModal.tsx
+++ frontend/src/components/GameOverModal.tsx
@@ -41,15 +41,46 @@ const GameOverModal: React.FC<GameOverModalProps> = ({
       console.error("Invalid game status");
   }
 
+  // Determine background color based on gameStatus and currentTurn
+  const backgroundColor = () => {
+    if (gameStatus === "draw") {
+      return "yellow";
+    } else if (gameStatus === "complete" && currentTurn === "X") {
+      return "red";
+    } else if (gameStatus === "complete" && currentTurn === "O") {
+      return "blue";
+    }
+    return ""; // Default background color if none of the conditions match
+  };
   return (
     <Dialog
       open={open}
+      maxWidth={"lg"}
       onClose={handleClose}
       aria-labelledby="win-tie-dialog-title"
       aria-describedby="win-tie-dialog-description"
+      sx={{
+        // Apply conditional background color
+        backgroundColor: backgroundColor(),
+      }}
     >
-      <DialogTitle id="win-tie-dialog-title">{title}</DialogTitle>
-      <DialogContent>
+      <DialogTitle
+        id="win-tie-dialog-title"
+        sx={{
+          // Apply conditional background color
+          textAlign: "center",
+          fontSize: "32px",
+        }}
+      >
+        {title}
+      </DialogTitle>
+      <DialogContent
+        sx={{
+          // Apply conditional background color
+          width: "300px",
+          textAlign: "center",
+        }}
+      >
         <DialogContentText id="win-tie-dialog-description">
           {contentText}
         </DialogContentText>
